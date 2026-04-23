16dfd0ef91272f24f75f4c7c273658f906c0a407
Kedolce <Kedolcegocek@gmail.com>
2026-04-23 12:55:30 +0700

    added mix blend feat in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="fixed left-0 right-0 z-50 flex items-center justify-between max-w-4xl px-8 py-3 mx-auto text-white border rounded-full top-8 border-white/5 bg-black/20 backdrop-blur-md">
+    <nav className="fixed left-0 right-0 z-50 flex items-center justify-between max-w-4xl px-8 py-3 mx-auto text-white border rounded-full mix-blend-difference top-8 border-white/5 bg-black/20 backdrop-blur-md">
       
       <h1 className="text-sm font-black tracking-tighter uppercase transition duration-300 cursor-pointer hover:text-slate-400">
         Kadek.dev
